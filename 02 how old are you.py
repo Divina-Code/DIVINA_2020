578df5858c6ba3199dad0fb3352f98f75f51c197
Ekaterina Divina <ekaterinadivina@gmail.com>
2020-12-27 17:45:27 +0300

    ex03

--- 02 how old are you.py
+++ 02 how old are you.py
@@ -1,11 +1,15 @@
 print("Hello, user!")
 
 user_name = input("What is your name?   ")
-print('Hello', user_name)
+print('Hello', user_name, "in your name ", len(user_name), 'letters')
 
 
 user_year = int( input("When you was born?  "))
-print("nice year,", user_year)
+if len(str(user_year)) == 4:
+    print("nice year,", user_year)
+    print(2020-user_year)
+else:
+    print("You make a little mistake")
 
+print("good bye")
 
-print(2020-user_year)
