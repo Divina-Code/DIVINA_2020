087b804aba89fb8eb8fa8f946803d5841b14f0bb
Ekaterina Divina <ekaterinadivina@gmail.com>
2020-12-27 18:41:03 +0300

    04 puzzle

--- 03 cinema.py
+++ 03 cinema.py
@@ -5,3 +5,19 @@
 ИНАЧЕ предложить ему комедию
 
 """
+
+user_name = input("What is your name    ")
+user_year = int(input("when you was born    "))
+
+if 2020 - user_year >= 18:
+    print(user_name+', you can watch a action movie')
+elif 2020 - user_year >= 16 :
+    print(user_name+', you can watch a horror movie')
+elif 2020 - user_year >= 13 :
+    print(user_name+', you can watch a comedy movie')
+else:
+    print(user_name+', you can watch a cartoon')
+
+
+
+
